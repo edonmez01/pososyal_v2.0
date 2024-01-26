608503eaecc22fd95b6759a60fc1bf4c84242c62
Ömer Talip Akalın <omertalib42@gmail.com>
2024-01-26 23:39:32 +0100

    Add sticky header

--- src/beautify.py
+++ src/beautify.py
@@ -3,6 +3,19 @@ from bs4 import BeautifulSoup
 
 def make_table_sortable():
     soup = BeautifulSoup(open("out.html").read(), "html.parser")
+    head = soup.head
+    if head is None:
+        head = soup.new_tag("head")
+        soup.html.insert(0, head)
+    style_tag = soup.new_tag('style')
+    style_tag.string = """
+    th {
+        position: sticky;
+        top: 0;
+        background-color: #fff;
+    }
+    """
+    head.append(style_tag)
     script_tag = soup.new_tag('script')
     script_tag['src'] = "https://www.kryogenix.org/code/browser/sorttable/sorttable.js"
     soup.append(script_tag)
